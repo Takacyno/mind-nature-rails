fda6833f09879d0e1a6ede086ee4b1eef7435327
taka2 <taka2001yuki@gmail.com>
2024-05-12 13:50:49 +0900

    deleted some files

--- app/javascript/application.js
+++ app/javascript/application.js
@@ -1,4 +1,16 @@
 // Configure your import map in config/importmap.rb. Read more: https://github.com/rails/importmap-rails
-// import "@hotwired/turbo-rails"
-import "controllers"
-import "../javascript/custom/observation.js"
+import "@hotwired/turbo-rails"
+window.textDisplayCheck=function(obj){
+    if(obj.checked){
+        obj.parentNode.nextElementSibling.classList.remove("hidden");
+    }else if(!obj.parentNode.nextElementSibling.classList.contains("hidden")){
+        obj.parentNode.nextElementSibling.classList.add("hidden");
+    }
+};
+window.textDisplaySelect=function(obj,displayNums){
+    if(displayNums.includes(Number(obj.value))){
+        obj.nextElementSibling.classList.remove("hidden");
+    }else if(!obj.nextElementSibling.classList.contains("hidden")){
+        obj.nextElementSibling.classList.add("hidden");
+    }
+};
